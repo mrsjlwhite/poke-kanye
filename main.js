1bf284e80d86c138dabfb3d95f32dcf2935825b2
Jeslyn <75355441+mrsjlwhite@users.noreply.github.com>
2024-02-07 10:31:25 -0800

    ✨ Added initial button func

---
+++ main.js
@@ -0,0 +1,13 @@
+const pokeAPI = 'https://pokeapi.co/api/v2/pokemon/';
+
+
+$(document).ready(function () {
+
+    $('.container').hide();
+
+
+    $('#surprise').click(function () {
+        $('.container').show();
+    });
+
+});
