1d46668524e298745b92b3f26516027c63af01e6
Jeslyn <75355441+mrsjlwhite@users.noreply.github.com>
2024-02-08 16:46:32 -0800

    ✨ Refactored api to include kanyes, new layout including both api results

--- main.js
+++ main.js
@@ -1,37 +1,46 @@
 const pokeAPI = 'https://pokeapi.co/api/v2/pokemon/';
+const kanyeAPI = 'https://api.kanye.rest';
 
 function capitalize(str) {
     return str[0].toUpperCase() + str.substring(1, str.length);
 }
 
+function getPokeMarkup(pokemon, quote) {
+    return `
+    <div class='poke-container'>
+        <div>
+            <div class="bubble">${quote}</div>
+            <div class="pointer"></div>
+        </div>
+        <div class='pokemon'>
+            <img src="${pokemon.sprites.front_shiny}" />
+            <p>${capitalize(pokemon.name)}</p>
+        </div>
+    </div>
+    `;
+}
+
 $(document).ready(function () {
 
-    $('.container').hide();
-
-
     $('#surprise').click(function () {
-        const randomPokemon = Math.ceil(Math.random() * 1017)
+        const randomPokemon = Math.ceil(Math.random() * 1017);
+        let quote = '';
 
-        fetch(`${pokeAPI}${randomPokemon}`)
+        fetch(kanyeAPI)
             .then(r => r.json())
-            .then(pokemon => {
-                console.log('pokemon', pokemon);
-                
-                $('.poke-container').remove();
+            .then(kanye => quote = kanye.quote)
+            .then(
+                fetch(`${pokeAPI}${randomPokemon}`)
+                    .then(r => r.json())
+                    .then(pokemon => {
+                        console.log('quote', quote);
+                        console.log('pokemon', pokemon);
 
-                const pokeMarkup = `
-                <div class='poke-container'>
-                    <img src="${pokemon.sprites.front_shiny}" />
-                    <p>${capitalize(pokemon.name)}</p>
-                </div>
-                `;
-
-                console.log('pokeMarkup', pokeMarkup.trim());
-
-                $('#pokemon').append(pokeMarkup);
-
-                $('.container').show();
-            })
+                        $('.poke-container').remove();
+                        $('#pokemon').append(getPokeMarkup(pokemon, quote));
+                        $('.container').show();
+                    })
+            )
 
     });
 
